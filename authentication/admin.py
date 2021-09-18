40dcd406f4a645d6b08c0605604364a56e9331e5
Gerard Nwazuruoke <NWAZUG01@heiway.net>
2021-09-18 23:29:20 +0100

    added admin model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
+
 
 # Register your models here.
+admin.site.register(User)
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+# admin.site.unregister(Group)
