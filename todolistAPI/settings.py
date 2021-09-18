8f47bdeacd6399e2631a27c52e8bcf73ca746cce
Gerard Nwazuruoke <NWAZUG01@heiway.net>
2021-09-18 21:32:27 +0100

    updated...

--- todolistAPI/settings.py
+++ todolistAPI/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-&h2ecb1*g34zk3sxr+s#=c&kck0j)he3pgg)2wz--!g&*dzka5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
